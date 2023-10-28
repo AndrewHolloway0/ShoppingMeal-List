860a19dd8843cc9b6a776fc22e98576f1ef21702
TwiddlesPenguin <andrew.k.holloway+git@hotmail.com>
2023-10-28 18:20:20 +0800

    fetch to axios

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,32 +1,58 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
+type ListOfMealsType = {
+  ID: number;
+  short_name: string;
+  name: string;
+  favourited: string;
+};
+
+type InitialListOfMealsType = {
+  initialListOfMeals: ListOfMealsType[];
+};
 
 export default function Home() {
   const [listOfMeals, setListOfMeals] = useState([{ ID: 0, short_name: "", name: "", favourited: "" }]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [err, setErr] = useState({ isErr: "false", errMsg: "" });
+
+  const getSelectedMeals = async() =>  {
+    setIsLoading(true);
+    axios
+      .get("http://192.168.1.90:3000/current")
+      .then((res) => {
+        setListOfMeals(res.data)
+      })
+      .catch((err) => {
+        setErr({ isErr: "true", errMsg: err.message })
+        // console.log(err.message)
+      })
+      .finally(() => {
+        setIsLoading(false)
+      });
+  };
 
   useEffect(() => {
-    fetch("http://192.168.1.90:3000/current", {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-    })
-    .then((res) => res.json())
-    .then((data) => {
-      // console.log(JSON.stringify(data))
-      setListOfMeals(data);
-    });
+    getSelectedMeals();
   }, []);
 
   return (
     <div className="contentContainer">
       <h1 className="pageTitle">Meal List</h1>
-      <Link className="btn btn_square" to="/newlist">Create New List</Link>
-      {listOfMeals.map((i) => (
-        <p>{i.name}</p>
-      ))}
+      <Link className="btn btn_square" to="/newlist">
+        Create New List
+      </Link>
+      <ul>
+        {isLoading ? (
+          <h4>Loading...</h4>
+        ) : err.isErr === "true" ? (
+          <p>An error occurred: {err.errMsg}</p>
+        ) : (
+          listOfMeals.map((i) => <li key={i.ID}>{i.name}</li>)
+        )}
+      </ul>
     </div>
   );
 }
--- client/src/pages/newlist.tsx
+++ client/src/pages/newlist.tsx
@@ -1,5 +1,8 @@
+"use client"
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
+import axios from "axios";
+// import { experimental_useFormStatus as useFormStatus } from "react-dom";
 
 // Import Styles
 import "../styles/home.scss";
@@ -13,13 +16,22 @@ export default function Home() {
     StepTwo(),
   ]);
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function onSubmit(data: FormData) {
+    setIsLoading(true)
+    await console.log(data.values())
+    setIsLoading(false)
+    // await console.log("Sumitted: "+ allData)
+  }
+
   return (
     <>
       <div className="contentContainer">
         <h1 className="pageTitle">New Weekly List</h1>
         <h3>Select the meals you would like to have</h3>
         <Link className="btn btn_square" to="/">View Current List</Link>
-        {currentStepIndex == 1 ? (
+        {currentStepIndex === 1 ? (
           <Link className="formArrows" to="" onClick={back} tabIndex={0}>
             &lt;
           </Link>
@@ -37,58 +49,74 @@ export default function Home() {
         <p>
           {currentStepIndex + 1} / {steps.length}
         </p>
-        <form className="optionList">{step}</form>
-        
+        <form className="optionList" action={onSubmit}>
+          {step}
+          <button disabled={isLoading}>
+            {isLoading ? "Loading..." : "Submit"}
+          </button>
+        </form>
       </div>
     </>
   );
 }
 
 export function StepOne() {
-  const [listOfFoods, setListOfFoods] = useState([{ ID: 0, short_name: "", name: "", favourited: "" }]);
-  const [currentList, setCurrentList] = useState([{ ID: 0, short_name: "", name: "", favourited: "" }]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [err, setErr] = useState({ isErr: "false", errMsg: "" });
+
+  const [listOfMeals, setListOfMeals] = useState([{ ID: 0, short_name: "", name: "", favourited: "" }]);
+  const [currentMealList, setCurrentMealList] = useState([{ ID: 0, short_name: "", name: "", favourited: "" }]);
+  const [newSelectedMeals, setNewSelectedMeals] = useState([]);
   
-  useEffect(() => {
-    fetch('http://192.168.1.90:3000/meals', {
-      method: "GET",
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-      },
-    })
-    .then((res) => (res.json()))
-    .then((data) => {
-      setListOfFoods(data)
-      // console.log(listOfFoods)
-    })
-  }, [])
+  const getAllMeals = async() =>  {
+    setIsLoading(true);
+    axios
+      .get("http://192.168.1.90:3000/meals")
+      .then((res) => {
+        setListOfMeals(res.data)
+      })
+      .catch((err) => {
+        setErr({ isErr: "true", errMsg: err.message })
+      })
+      .finally(() => {
+        getSelectedMeals()
+      });
+  };
+  
+  const getSelectedMeals = async() =>  {
+    axios
+      .get("http://192.168.1.90:3000/meals")
+      .then((res) => {
+        setCurrentMealList(res.data)
+      })
+      .catch((err) => {
+        setErr({ isErr: "true", errMsg: err.message })
+        // console.log(err.message)
+      })
+      .finally(() => {
+        setIsLoading(false)
+      });
+  };
 
   useEffect(() => {
-    fetch('http://192.168.1.90:3000/current', {
-      method: "GET",
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-      },
-    })
-    .then((res) => (res.json()))
-    .then((data) => {
-      setCurrentList(data)
-      // console.log(currentList)
-    })
-  }, [listOfFoods])
+    getAllMeals();
+  }, []);
   
   return (
     <>
-      {listOfFoods.map((i) => (
-        <div key={i.ID} className="checkboxOption">
-          {/* <input checked={currentList.includes(i)} type="checkbox" id={i.short_name} /> */}
-          <input type="checkbox" id={i.short_name} />
-          <label htmlFor={i.short_name}>{i.name}</label>
-        </div>
-      ))}
+      {isLoading ? (
+        <h4>Loading...</h4>
+      ) : err.isErr == "true" ? (
+        <p>An error occurred: {err.errMsg}</p>
+      ) : (
+        listOfMeals.map((i) => (
+          <div key={i.ID} className="checkboxOption">
+            {/* <input checked={currentList.includes(i)} type="checkbox" id={i.short_name} /> */}
+            <input type="checkbox" id={i.short_name} />
+            <label htmlFor={i.short_name}>{i.name}</label>
+          </div>
+        ))
+      )}
     </>
   );
 }
